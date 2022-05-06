6d9bf365e65577bc0c828bb32b05f1b8e6b8c2d6
Thalles Alexsander <thallesander@hotmail.com>
2022-05-05 23:00:52 -0300

    set objects in function widget form

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -29,8 +29,10 @@ const feedbackTypes = {
     },
 };
 
+type feedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
-    const [] = useState()
+    const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null)
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -40,13 +42,14 @@ export function WidgetForm() {
                 <CloseButton/>
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
+            {!feedbackType ? (
+                <div className="flex py-8 gap-2 w-full">
                 { Object.entries(feedbackTypes).map(([key, value]) => {
                     return (
                         <button
                             key={key}
                             className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            onClick={}
+                            onClick={() => setFeedbackType(key as feedbackType)}
                         >
                             <img src={value.image.source} alt={value.image.alt} />
                             <span>{value.title}</span>
@@ -54,6 +57,9 @@ export function WidgetForm() {
                     )
                 })}
             </div>
+            ) : (
+                <p>Hello World</p>
+            )}
 
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ por <a className="underline underline-offset-2" href="https://github.com/Alezuzzo">Alezuzzo</a>
