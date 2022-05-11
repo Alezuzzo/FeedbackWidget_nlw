6ed77dde7016a90bdb42e48aed8336c6a2d2ca34
Thalles Alexsander <thallesander@hotmail.com>
2022-05-10 23:13:03 -0300

    adding success for forms

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,3 +1,28 @@
-export function FeedbackSuccessStep() {
-    return ()
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackSuccessStepProps {
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({onFeedbackRestartRequested}: FeedbackSuccessStepProps) {
+    return (
+        <>
+        <header>
+            <CloseButton />>
+        </header>
+
+        <div>
+
+        </div>
+
+        <span className="text-xl mt-2">Agradecemos o feedback!</span>
+
+        <button type="button"
+                onClick={onFeedbackRestartRequested}
+                className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500">
+            Quero enviar outro
+        </button>
+
+        </>
+    )
 }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import thoughtImageUrl from '../../assets/thought.svg';
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -35,21 +36,33 @@ export type feedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null)
+    const [feedbackSent, sentFeedbackSent] = useState(false);
 
     function handleRestartFeedback() {
+        sentFeedbackSent(false);
         setFeedbackType(null);
+        
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
             ) : (
-                <FeedbackContentStep feedbackType={feedbackType}
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                    ) : (
+                        <FeedbackContentStep feedbackType={feedbackType}
                                     onFeedbackRestartRequested={handleRestartFeedback}
-                />
-            )}
+                                    onFeedbackSent={() => sentFeedbackSent(true)}
+                        />
+                    )}
+                </>
+            )
+
+            }
+            
 
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ por <a className="underline underline-offset-2" href="https://github.com/Alezuzzo">Alezuzzo</a>
